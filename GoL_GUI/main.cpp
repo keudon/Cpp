9369df4b6e275c8f77143237b32280ae59000c14
keudon <62550524+keudon@users.noreply.github.com>
2021-03-04 21:56:51 +0100

    GameOfLife 1st rectangle drawn

--- GoL_GUI/main.cpp
+++ GoL_GUI/main.cpp
@@ -7,6 +7,8 @@
 #include <tchar.h>
 #include <windows.h>
 #include <iostream>
+#include <gdiplus.h>
+using namespace Gdiplus;
 
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
@@ -22,6 +24,11 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     HWND hwnd;               /* This is the handle for our window */
     MSG messages;            /* Here messages to the application are saved */
     WNDCLASSEX wincl;        /* Data structure for the windowclass */
+
+    /* Initialize GDI+ */
+    GdiplusStartupInput gdiplusstartupinput;
+    ULONG_PTR gdiplustoken;
+    GdiplusStartup(&gdiplustoken, &gdiplusstartupinput, NULL);
 
     /* The Window structure */
     wincl.hInstance = hThisInstance;
@@ -72,6 +79,9 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
         DispatchMessage(&messages);
     }
 
+    /* End GDI+ */
+    GdiplusShutdown(gdiplustoken);
+
     /* The program return-value is 0 - The value that PostQuitMessage() gave */
     return messages.wParam;
 }
@@ -81,11 +91,17 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 
 LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+
+    PAINTSTRUCT ps; // Paint Data
+    HDC hdc; // Handle to Device Context
+
     switch (message)                  /* handle the messages */
     {
+
         case WM_DESTROY:
             PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
             break;
+
         case WM_CLOSE:
             if (MessageBox(hwnd,_T("REALLY QUIT?"),_T("DFQ IS THAT"),MB_OKCANCEL) == IDOK)
             {
@@ -96,8 +112,61 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 
         case WM_LBUTTONDOWN:
             std::cout << "LEFT CLICKED !" << std::endl;
+
         case WM_PAINT:
             std::cout << "Paint requested" << std::endl;
+
+            hdc = BeginPaint(hwnd,&ps);
+
+//            HPEN hpenOld;
+//            hpenOld = static_cast<HPEN>(SelectObject(hdc, GetStockObject(DC_PEN)));
+//            HBRUSH hbrushOld;
+//            hbrushOld = static_cast<HBRUSH>(SelectObject(hdc, GetStockObject(NULL_BRUSH)));
+
+               // Calculate the dimensions of the 4 equal rectangles.
+//            RECT rcWindow;
+//            GetClientRect(hwnd, &rcWindow);
+
+//            RECT rc1, rc2, rc3, rc4;
+//            rc1 = rc2 = rc3 = rc4 = rcWindow;
+
+//            rc1.right  -= (rcWindow.right - rcWindow.left) / 2;
+//            rc1.bottom -= (rcWindow.bottom - rcWindow.top) / 2;
+
+//            rc2.left   = rc1.right;
+//            rc2.bottom = rc1.bottom;
+
+//            rc3.top   = rc1.bottom;
+//            rc3.right = rc1.right;
+
+//            rc4.top  = rc1.bottom;
+//            rc4.left = rc1.right;
+
+            // Optionally, deflate each of the rectangles by an arbitrary amount so that
+            // they don't butt up right next to each other and we can distinguish them.
+//            InflateRect(&rc1, -5, -5);
+//            InflateRect(&rc2, -5, -5);
+//            InflateRect(&rc3, -5, -5);
+//            InflateRect(&rc4, -5, -5);
+
+            SetDCPenColor(hdc, RGB(255, 0, 0));    // red
+
+            Rectangle(hdc, 0, 0, 100, 100);
+
+//            std::cout << "left = " << rc1.left << std::endl;
+//            std::cout << "top = " << rc1.top << std::endl;
+//            std::cout << "right = " << rc1.right << std::endl;
+//            std::cout << "bottom = " << rc1.bottom << std::endl;
+
+//            FillRect(hdc,&ps.rcPaint,(HBRUSH) (COLOR_WINDOW+2));
+
+//            SolidBrush redBrush(Color::Red);
+//            Pen greenPen(Color::Green, 2.0);
+//            g.FillRectangle(&redBrush, 20, 20, 100, 100);
+//            g.DrawEllipse(&greenPen, 30, 30, 80, 80);
+            EndPaint(hwnd, &ps);
+            return 0;
+
         default:                      /* for messages that we don't deal with */
             return DefWindowProc (hwnd, message, wParam, lParam);
     }
