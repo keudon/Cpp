b98c4a4b96799b2482845ad2b4f980b6a5f154be
keudon <62550524+keudon@users.noreply.github.com>
2021-03-12 23:41:34 +0100

    Init and repaint differenciated

--- GoL_GUI/main.cpp
+++ GoL_GUI/main.cpp
@@ -1,7 +1,7 @@
 #if defined(UNICODE) && !defined(_UNICODE)
-    #define _UNICODE
+#define _UNICODE
 #elif defined(_UNICODE) && !defined(UNICODE)
-    #define UNICODE
+#define UNICODE
 #endif
 
 #include <tchar.h>
@@ -17,9 +17,9 @@ LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 TCHAR szClassName[ ] = _T("CodeBlocksWindowsApp");
 
 int WINAPI WinMain (HINSTANCE hThisInstance,
-                     HINSTANCE hPrevInstance,
-                     LPSTR lpszArgument,
-                     int nCmdShow)
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpszArgument,
+                    int nCmdShow)
 {
     HWND hwnd;               /* This is the handle for our window */
     MSG messages;            /* Here messages to the application are saved */
@@ -45,7 +45,7 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     wincl.cbClsExtra = 0;                      /* No extra bytes after the window class */
     wincl.cbWndExtra = 0;                      /* structure or the window instance */
     /* Use Windows's default colour as the background of the window */
-    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND;
+    wincl.hbrBackground = (HBRUSH) COLOR_BACKGROUND+1;
 
     /* Register the window class, and if it fails quit the program */
     if (!RegisterClassEx (&wincl))
@@ -53,18 +53,18 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 
     /* The class is registered, let's create the program*/
     hwnd = CreateWindowEx (
-           0,                   /* Extended possibilites for variation */
-           szClassName,         /* Classname */
-           _T("Code::Blocks Template Windows App"),       /* Title Text */
-           WS_OVERLAPPEDWINDOW, /* default window */
-           CW_USEDEFAULT,       /* Windows decides the position */
-           CW_USEDEFAULT,       /* where the window ends up on the screen */
-           544,                 /* The programs width */
-           375,                 /* and height in pixels */
-           HWND_DESKTOP,        /* The window is a child-window to desktop */
-           NULL,                /* No menu */
-           hThisInstance,       /* Program Instance handler */
-           NULL                 /* No Window Creation data */
+               0,                   /* Extended possibilites for variation */
+               szClassName,         /* Classname */
+               _T("Code::Blocks Template Windows App"),       /* Title Text */
+               WS_OVERLAPPEDWINDOW, /* default window */
+               CW_USEDEFAULT,       /* Windows decides the position */
+               CW_USEDEFAULT,       /* where the window ends up on the screen */
+               544,                 /* The programs width */
+               375,                 /* and height in pixels */
+               HWND_DESKTOP,        /* The window is a child-window to desktop */
+               NULL,                /* No menu */
+               hThisInstance,       /* Program Instance handler */
+               NULL                 /* No Window Creation data */
            );
 
     /* Make the window visible on the screen */
@@ -86,90 +86,103 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     return messages.wParam;
 }
 
+void repaint_square(HWND hwnd,LPRECT pmyrect,PAINTSTRUCT ps,HBRUSH the_brush);
 
 /*  This function is called by the Windows function DispatchMessage()  */
+HDC hdc; // Handle to Device Context
+PAINTSTRUCT ps; // Paint Data
+static RECT myrect;
+LPRECT prect;
+POINT mypt;
 
 LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 
-    PAINTSTRUCT ps; // Paint Data
-    HDC hdc; // Handle to Device Context
-
     switch (message)                  /* handle the messages */
     {
 
-        case WM_DESTROY:
-            PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
-            break;
+    case WM_DESTROY:
+        PostQuitMessage (0);       /* send a WM_QUIT to the message queue */
+        break;
 
-        case WM_CLOSE:
-            if (MessageBox(hwnd,_T("REALLY QUIT?"),_T("DFQ IS THAT"),MB_OKCANCEL) == IDOK)
-            {
-                DestroyWindow(hwnd);
-            } else {
-                return 0;
-            }
+    case WM_CLOSE:
+//        if (MessageBox(hwnd,_T("REALLY QUIT?"),_T("DFQ IS THAT"),MB_OKCANCEL) == IDOK)
+//        {
+        DestroyWindow(hwnd);
+//        }
+//        else
+//        {
+        return 0;
+//        }
 
-        case WM_LBUTTONDOWN:
-            std::cout << "LEFT CLICKED !" << std::endl;
+//        case WM_LBUTTONUP:
+//            std::cout << "Up clicked !" << std::endl;
+//            SetRect(&myrect,0,0,100,100);
 
-        case WM_PAINT:
-            std::cout << "Paint requested" << std::endl;
 
-            hdc = BeginPaint(hwnd,&ps);
+    case WM_LBUTTONDOWN:
+//        std::cout << "WM_LBUTTONDOWN" << std::endl;
 
-//            HPEN hpenOld;
-//            hpenOld = static_cast<HPEN>(SelectObject(hdc, GetStockObject(DC_PEN)));
-//            HBRUSH hbrushOld;
-//            hbrushOld = static_cast<HBRUSH>(SelectObject(hdc, GetStockObject(NULL_BRUSH)));
+        mypt.x = (LONG) LOWORD(lParam);
+        mypt.y = (LONG) HIWORD(lParam);
 
-               // Calculate the dimensions of the 4 equal rectangles.
-//            RECT rcWindow;
-//            GetClientRect(hwnd, &rcWindow);
+        if (PtInRect(&myrect,mypt))
+        {
+            std::cout << "In RECT !" << std::endl;
 
-//            RECT rc1, rc2, rc3, rc4;
-//            rc1 = rc2 = rc3 = rc4 = rcWindow;
+            hdc = GetDC(hwnd);
+            static RECT mynewrect;
+            HBRUSH my_red_brush;
+            my_red_brush = CreateSolidBrush(RGB(255,0,0));
+            SetRect(&mynewrect,150,150,200,200);
+            FillRect(hdc,&mynewrect,my_red_brush);
+            DeleteObject(my_red_brush);
+            ReleaseDC(hwnd,hdc);
 
-//            rc1.right  -= (rcWindow.right - rcWindow.left) / 2;
-//            rc1.bottom -= (rcWindow.bottom - rcWindow.top) / 2;
+        }
+        else
+        {
+            std::cout << "NOT in RECT :( " << std::endl;
+        }
 
-//            rc2.left   = rc1.right;
-//            rc2.bottom = rc1.bottom;
+        return 0;
 
-//            rc3.top   = rc1.bottom;
-//            rc3.right = rc1.right;
+    case WM_PAINT:
 
-//            rc4.top  = rc1.bottom;
-//            rc4.left = rc1.right;
+        HDC hdc;
+        HBRUSH my_green_brush;
+        my_green_brush = CreateSolidBrush(RGB(0,255,0));
+        hdc = BeginPaint(hwnd,&ps);
+        SetRect(&myrect,0,0,150,150);
+        SelectObject(hdc,my_green_brush);
+        FillRect(hdc,&myrect,my_green_brush);
+        DeleteObject(my_green_brush);
+        EndPaint(hwnd,&ps);
+        return 0;
 
-            // Optionally, deflate each of the rectangles by an arbitrary amount so that
-            // they don't butt up right next to each other and we can distinguish them.
-//            InflateRect(&rc1, -5, -5);
-//            InflateRect(&rc2, -5, -5);
-//            InflateRect(&rc3, -5, -5);
-//            InflateRect(&rc4, -5, -5);
-
-            SetDCPenColor(hdc, RGB(255, 0, 0));    // red
-
-            Rectangle(hdc, 0, 0, 100, 100);
-
-//            std::cout << "left = " << rc1.left << std::endl;
-//            std::cout << "top = " << rc1.top << std::endl;
-//            std::cout << "right = " << rc1.right << std::endl;
-//            std::cout << "bottom = " << rc1.bottom << std::endl;
-
-//            FillRect(hdc,&ps.rcPaint,(HBRUSH) (COLOR_WINDOW+2));
-
-//            SolidBrush redBrush(Color::Red);
-//            Pen greenPen(Color::Green, 2.0);
-//            g.FillRectangle(&redBrush, 20, 20, 100, 100);
-//            g.DrawEllipse(&greenPen, 30, 30, 80, 80);
-            EndPaint(hwnd, &ps);
-            return 0;
-
-        default:                      /* for messages that we don't deal with */
-            return DefWindowProc (hwnd, message, wParam, lParam);
+    default:                      /* for messages that we don't deal with */
+        return DefWindowProc (hwnd, message, wParam, lParam);
     }
 
     return 0;
+}
+
+void repaint_square(HWND hwnd,LPRECT pmyrect,PAINTSTRUCT ps,HBRUSH the_brush)
+{
+
+
+//    hdc = GetDC(hwnd);
+//    Rectangle(hdc,pmyrect->left,pmyrect->top,pmyrect->right,pmyrect->bottom);
+
+//    Rectangle(hdc,pmyrect->left,pmyrect->top,pmyrect->right,pmyrect->bottom);
+//    if(test==0) {
+
+//    } else {
+//        SetRect(pmyrect,150,150,150,200);
+//    }
+
+//    ReleaseDC(hwnd,hdc);
+
+
+
 }
