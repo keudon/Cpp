a968b1e75f3ead042972d323cde7b110049a3b19
keudon <62550524+keudon@users.noreply.github.com>
2021-03-21 17:50:22 +0100

    Comment cleaning

--- GoL_GUI/main.cpp
+++ GoL_GUI/main.cpp
@@ -12,17 +12,6 @@
 
 using namespace Gdiplus;
 
-//class CELL_BOARD
-//{
-//public:
-//    int n_cells;
-//    bool cell_is_modified=false;
-//private:
-//    static LRESULT CALLBACK WindowProcedure( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
-//    static const char * m_pszClassName;
-//    HWND m_hWnd;
-//};
-
 /*  Declare Windows procedure  */
 LRESULT CALLBACK WindowProcedure (HWND, UINT, WPARAM, LPARAM);
 
@@ -41,9 +30,6 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
                     LPSTR lpszArgument,
                     int nCmdShow)
 {
-//    CELL_BOARD the_cell;
-//    the_cell.n_cells = 10;
-
     HWND hwnd;               /* This is the handle for our window */
     MSG messages;            /* Here messages to the application are saved */
     WNDCLASSEX wincl;        /* Data structure for the windowclass */
@@ -75,15 +61,10 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     if (!RegisterClassEx (&wincl))
         return 0;
 
-    /* The class is registered, let's create the program*/
-//    CELL_BOARD * pcellboard = new CELL_BOARD;
-//    RECT my_array_of_rect[10][10];
-//    RECT my_rect;
-//    LPRECT p_to_my_rect;
-//    p_to_my_rect=&my_rect;
 
     Plateau *p_plateau = new Plateau;
 
+    /* The class is registered, let's create the program*/
     hwnd = CreateWindowEx (
                0,                   /* Extended possibilites for variation */
                szClassName,         /* Classname */
@@ -122,8 +103,6 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
 bool is_vert,rect_is_modified;
 int cell_size=10;
 int taille[2]= {10,10};
-//RECT my_array_of_rect[taille[0]][taille[1]];
-//std::cout << "WHERE AM I ?" << std::endl;
 static RECT my_array_of_rect[10][10];
 //Board the_board;
 
@@ -160,10 +139,12 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
         p_plateau = reinterpret_cast<Plateau*>(pCreate->lpCreateParams);
         SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)p_plateau);
+
         std::cout << "Board size x ?" << std::endl;
         std::cin >> p_plateau->size_x;
         std::cout << "Board size y ?" << std::endl;
         std::cin >> p_plateau->size_y;
+
         p_plateau->ready= false;
 
         p_plateau->cells = new RECT * [p_plateau->size_x];
@@ -172,27 +153,11 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
         {
             p_plateau->cells[i] = new RECT [p_plateau->size_y];
         }
-
-
-
-//        tmp
-//        p_plateau->p_cells = new RECT [p_plateau->size_x][p_plateau->size_y];
-//        int taille[2]= {p_plateau->size_x, p_plateau->size_y};
-//        RECT my_array_of_rect[p_plateau->size_x][p_plateau->size_y];
-
     }
     else
     {
         ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
         p_plateau = reinterpret_cast<Plateau*>(ptr);
-
-//        if (pState->is_green == 1) {
-//                std::cout << "pState->is_green: true" << std::endl;
-//        }
-//        else
-//        {
-//                std::cout << "pState->is_green: false" << std::endl;
-//        }
 
     }
 
@@ -219,21 +184,9 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 
 
     case WM_LBUTTONDOWN:
-//        toto = (*pState).is_green;
-//        std::cout << "(*pState).is_green = " << toto << std::endl;
-
-//        LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
-//        StateInfo *pState = reinterpret_cast<StateInfo*>(ptr);
-
-//        std::cout << "pState : " << pState << std::endl;
-
-//        std::cout << "WM_LBUTTONDOWN" << std::endl;
 
         mypt.x = (LONG) LOWORD(lParam);
         mypt.y = (LONG) HIWORD(lParam);
-
-//        std::cout << "mypt.x=" << mypt.x << std::endl;
-//        std::cout << "mypt.y=" << mypt.y << std::endl;
 
         rect_is_modified = false;
 
@@ -243,15 +196,6 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 
                 if (!rect_is_modified && PtInRect(&my_array_of_rect[i][j],mypt))
                 {
-//                    std::cout << "left" << my_array_of_rect[i][j].left << std::endl;
-//                    std::cout << "top" << my_array_of_rect[i][j].top << std::endl;
-//                    std::cout << "right" << my_array_of_rect[i][j].right << std::endl;
-//                    std::cout << "bottom" << my_array_of_rect[i][j].bottom << std::endl;
-//
-                    std::cout << "found" << std::endl;
-//                    std::cout << "i=" << i << std::endl;
-//                    std::cout << "j=" << j << std::endl;
-
 
                     hdc = GetDC(hwnd);
                     if (is_vert)
@@ -276,14 +220,6 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
 
     case WM_CREATE:
 
-
-
-//        CREATESTRUCT * pcs;
-//        pcs = reinterpret_cast<CREATESTRUCT*>(lParam);
-
-//        final_p_rect = reinterpret_cast<LPRECT>(pcs->lpCreateParams);
-//        SetWindowLongPtrA(hwnd,GWLP_USERDATA,(LONG_PTR)final_p_rect);
-
         HDC hdc;
         hdc = BeginPaint(hwnd,&ps);
         SelectObject(hdc,my_green_brush);
@@ -302,11 +238,6 @@ LRESULT CALLBACK WindowProcedure (HWND hwnd, UINT message, WPARAM wParam, LPARAM
             m=m+cell_size+1;
 
         }
-
-//        std::cout << "final_p_rect->bottom = " << final_p_rect->bottom << std::endl;
-//        SetRect(final_p_rect,50,50,100,100);
-//        FillRect(hdc,final_p_rect,my_green_brush);
-
 
         EndPaint(hwnd,&ps);
         is_vert = true;
