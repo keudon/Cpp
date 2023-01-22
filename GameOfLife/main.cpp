40da8d475a417709a78ebeaf8bc38dbe03ffa667
keudon <62550524+keudon@users.noreply.github.com>
2023-01-22 13:19:26 +0100

    creation of function "update_cells"

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -29,6 +29,17 @@ int cell_size=10;
 
 //DWORD WINAPI ThreadFun1(HWND,LPVOID);
 
+int update_cells(int cell_alive_next_round,int cell_alive,RECT cell_geometry,HDC hdc,HBRUSH my_red_brush,HBRUSH my_green_brush)
+{
+int cell_status = cell_alive;
+if (cell_alive_next_round != cell_alive)
+{
+    cell_status = cell_alive_next_round;
+    (cell_status) ? FillRect(hdc,&cell_geometry,my_red_brush) : FillRect(hdc,&cell_geometry,my_green_brush);
+}
+return cell_status;
+}
+
 int WINAPI WinMain (HINSTANCE hThisInstance,
                     HINSTANCE hPrevInstance,
                     LPSTR lpszArgument,
@@ -126,13 +137,8 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
                     p_board->p_cell->alive_next_round[x][y] = cell_alive_next_round;
 
                     // Make the update
-                    if (p_board->p_cell->alive_next_round[x][y] != p_board->p_cell->alive[x][y])
-                    {
-                        p_board->p_cell->alive[x][y] = p_board->p_cell->alive_next_round[x][y];
+                    p_board->p_cell->alive[x][y] = update_cells(p_board->p_cell->alive_next_round[x][y],p_board->p_cell->alive[x][y],(p_board->p_cell->geometry)[x][y],hdc,my_red_brush,my_green_brush);
 
-                        (p_board->p_cell->alive[x][y]) ? FillRect(hdc,&(p_board->p_cell->geometry)[x][y],my_red_brush) : FillRect(hdc,&(p_board->p_cell->geometry)[x][y],my_green_brush);
-
-                    }
                 }
             }
 
