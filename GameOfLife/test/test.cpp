ecfdc9f87f7e29fc9efeef7683993876df68bf7f
keudon <62550524+keudon@users.noreply.github.com>
2023-01-23 20:36:20 +0100

    test and CMake updated for testing update_cell

--- GameOfLife/test/test.cpp
+++ GameOfLife/test/test.cpp
@@ -1,9 +1,17 @@
 #include <gtest/gtest.h>
+#include "../src/board.h"
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(Game_Of_Life_Test, Update_Cell_Test) {
+  int cell_alive_next_round = 0;
+  int cell_alive = 1;
+  HDC hdc; // Handle to Device Context
+  HBRUSH my_red_brush, my_green_brush;
+  RECT cell_geometry;
+
+  int res;
+
+  res = update_cells(cell_alive_next_round,cell_alive,cell_geometry,hdc,my_red_brush,my_green_brush);
+  
+  EXPECT_EQ(res, 0);
 }
