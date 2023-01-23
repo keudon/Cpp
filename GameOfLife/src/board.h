be36d0e19b343443c80334161f7bc11de75acef2
keudon <62550524+keudon@users.noreply.github.com>
2023-01-23 20:35:01 +0100

    update_cell function moved into board.cpp

--- GameOfLife/src/board.h
+++ GameOfLife/src/board.h
@@ -13,3 +13,5 @@ public:
     int number_of_rounds;
     bool game_on {false};
 };
+
+int update_cells(int cell_alive_next_round,int cell_alive,RECT cell_geometry,HDC hdc,HBRUSH my_red_brush,HBRUSH my_green_brush);
--- GameOfLife/src/main.cpp
+++ GameOfLife/src/main.cpp
@@ -29,17 +29,6 @@ int cell_size=10;
 
 //DWORD WINAPI ThreadFun1(HWND,LPVOID);
 
-int update_cells(int cell_alive_next_round,int cell_alive,RECT cell_geometry,HDC hdc,HBRUSH my_red_brush,HBRUSH my_green_brush)
-{
-int cell_status = cell_alive;
-if (cell_alive_next_round != cell_alive)
-{
-    cell_status = cell_alive_next_round;
-    (cell_status) ? FillRect(hdc,&cell_geometry,my_red_brush) : FillRect(hdc,&cell_geometry,my_green_brush);
-}
-return cell_status;
-}
-
 int WINAPI WinMain (HINSTANCE hThisInstance,
                     HINSTANCE hPrevInstance,
                     LPSTR lpszArgument,
